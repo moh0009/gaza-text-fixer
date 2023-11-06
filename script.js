d733ceca031ec5b975b3e68c9b70184a53268a54
moh0009 <83363934+moh0009@users.noreply.github.com>
2023-11-06 04:24:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
-var text = document.querySelector("#text1");
-var val  = text.value;
-var button = document.getElementById("button");
-button.onclick = con;
-
-var con_text = document.getElementById("con_text");
-
-console.log(con_text);
-
-
-function con(){
-    var sp = text.value.split("");
-    con_text.innerText = sp;
-    for(let i = 0; i <= sp; i++){
-        console.log("hi")
-    }    
-}
+document.addEventListener("DOMContentLoaded", function() {
+    var text = document.querySelector("#text1");
+    var button = document.getElementById("button");
+    var con_text = document.getElementById("con_text");
+  
+    button.addEventListener("click", con);
+  
+    function con() {
+      var wordReplacements = {
+        "gaza":"g_A_za",
+        ""
+      };
+  
+      var inputText = text.value;
+      var inputWords = inputText.split(" ");
+  
+      for (let i = 0; i < inputWords.length; i++) {
+        if (wordReplacements[inputWords[i]]) {
+          inputWords[i] = wordReplacements[inputWords[i]];
+        }
+      }
+  
+      con_text.innerText = inputWords.join(" ");
+    }
+  });  
